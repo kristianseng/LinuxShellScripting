3e91894bf5343cf75480f6b23a9855d8e222605a
kristianseng <62358534+kristianseng@users.noreply.github.com>
2021-08-22 22:55:44 +0200

    Add files via upload

---
+++ username_password.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+# This script creates an account on the local system.
+# You will be prompted for the account name and password.
+
+# Ask for the user name.
+read -p 'Enter the username to create: ' USER_NAME
+
+# Ask for the real name.
+read -p 'Enter the name of the person who this account is for: ' COMMENT
+
+# Ask for the password.
+read -p 'Enter the password to use for the account: ' PASSWORD
+
+# Create the user.
+useradd -c "${COMMENT}" -m ${USER_NAME}
+
+# Set the password for the user.
+# NOTE: You can also use the following command:
+#    echo "${USER_NAME}:${PASSWORD}" | chpasswd
+echo ${PASSWORD} | passwd --stdin ${USER_NAME}
+
+# Force password change on first login.
+passwd -e ${USER_NAME}
+
+
+#!/bin/bash
+
+# This script generates a list of random passwords.
+
+# A random number as a password.
+PASSWORD="${RANDOM}"
+echo "${PASSWORD}"
+
+# Three random numbers together.
+PASSWORD="${RANDOM}${RANDOM}${RANDOM}"
+echo "${PASSWORD}"
+
+# Use the current date/time as the basis for the password.
+PASSWORD=$(date +%s)
+echo "${PASSWORD}"
+
+# Use nanoseconds to act as randomization.
+PASSWORD=$(date +%s%N)
+echo "${PASSWORD}"
+
+# A better password.
+PASSWORD=$(date +%s%N | sha256sum | head -c32)
+echo "${PASSWORD}"
+
+# An even better password.
+PASSWORD=$(date +%s%N${RANDOM}${RANDOM} | sha256sum | head -c48)
+echo "${PASSWORD}"
+
+# Append a special character to the password.
+SPECIAL_CHARACTER=$(echo '!@#$%^&*()_-+=' | fold -w1 | shuf | head -c1)
+echo "${PASSWORD}${SPECIAL_CHARACTER}"
